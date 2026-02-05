5e3062ba3ccf86bdad19817e045fb64548fb0600
Rashmika Siriwardhana <58144396+rnsrashmika078@users.noreply.github.com>
2026-02-06 02:17:48 +0530

    change route code

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,20 +3,25 @@ import { groq } from "@ai-sdk/groq";
 import { createFile, weatherTool } from "@/app/helpers/tools";
 
 export async function POST(req: Request) {
+  try {
     const { messages }: { messages: UIMessage[] } = await req.json();
 
     console.log("messages", messages);
     const result = streamText({
-        // model: groq("openai/gpt-oss-20b"),
-        model: groq("llama-3.3-70b-versatile"),
-        system: "When you call a tool, you MUST answer the user using the tool result.",
-        tools: {
-            weatherTool,
-            createFile,
-        },
-        messages: await convertToModelMessages(messages),
+      // model: groq("openai/gpt-oss-20b"),
+      model: groq("llama-3.3-70b-versatile"),
+      system:
+        "When you call a tool, you MUST answer the user using the tool result.",
+      tools: {
+        weatherTool,
+        createFile,
+      },
+      messages: await convertToModelMessages(messages),
     });
     console.log("result", result);
 
     return result.toUIMessageStreamResponse();
+  } catch (err) {
+    console.log(err);
+  }
 }
